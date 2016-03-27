9cc70661ceaf868cbc02175145079a834d31b662
jackiewais <jackiewais@hotmail.com>
2016-03-27 20:44:33 -0300

    Server: se arman queues por client
    Se armar queues por client que se devuelven en el processWriting.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <map>
 #include <sys/socket.h>
-#include "SDL/SDL_thread.h"
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include<sys/errno.h>
+
+#include "SDL2/SDL_thread.h"
 
 #include <string.h>
 #include <stdio.h>
@@ -12,13 +19,40 @@
 
 using namespace std;
 
-int cantCon;
+int cant_con;
+map<int,int> socket_queue;
 
+struct msg_buf {
+  long mtype;
+  char mtext[200];
+};
 
-static int doProcessing (void *sockfd) {
+extern int errno;       // error NO.
+
+int writeQueueMessage(int socket, int msgid, char* message){
+	msg_buf msg;
+	int msgqid, rc;
+
+	// message to send
+	msg.mtype = msgid; // set the type of message
+	sprintf (msg.mtext, "%s\n", message); /* setting the right time format by means of ctime() */
+
+	msgqid = socket_queue[socket];
+	// send the message to queue
+	rc = msgsnd(msgqid, &msg, sizeof(msg.mtext), 0); // the last param can be: 0, IPC_NOWAIT, MSG_NOERROR, or IPC_NOWAIT|MSG_NOERROR.
+	if (rc < 0) {
+		perror( strerror(errno) );
+		printf("msgsnd failed, rc = %d\n", rc);
+	}
+
+	return rc;
+
+}
+static int doReading (void *sockfd) {
    int n;
    bool finish = false;
    char buffer[256];
+
 
    int sock = *(int*)sockfd;
 
@@ -30,25 +64,26 @@ static int doProcessing (void *sockfd) {
     	 	n = read(sock,buffer,255 );
     	 	if (n < 0) {
     	 	  perror("ERROR reading from socket \n");
-    	 	  //exit(1);
     	 	  finish = true;
     	 	}
 
     	 	if (n == 0){
     	 		//Exit message
     	 		printf("Exit message received \n");
+				writeQueueMessage(sock,99, "Exit message");
     	 		finish = true;
-    	 		cantCon--;
+    	 		cant_con--;
     	 	}else{
-
 				printf("Here is the message: %s \n",buffer);
 
 				//Respond to the client
 				n = write(sock,"I got your message \n",21);
 				if (n < 0) {
 				  perror("ERROR writing to socket \n");
-				  exit(1);
+				  finish = true;
 				 }
+
+				writeQueueMessage(sock,1, buffer);
     	 	}
      }
 
@@ -57,22 +92,77 @@ static int doProcessing (void *sockfd) {
 	return 0;
 }
 
+static int doWriting (void *sockfd) {
+   int n, rc;
+   bool finish = false;
+   msg_buf msg;
 
-int main()
-{
-    cout << "Starting...\n";
+   int sock = *(int*)sockfd;
 
-	int sockfd, newsockfd, portno, *new_sock;
-	socklen_t clilen;
-	struct sockaddr_in serv_addr, cli_addr;
-	int maxCon;
-	cantCon = 0;
+   int msgqid;
 
+   //Receive a message from client
+     while(!finish)
+     {
+		msgqid = socket_queue[sock];
+		rc = msgrcv(msgqid, &msg, sizeof(msg.mtext), 0, 0);
+		if (rc < 0) {
+			perror( strerror(errno) );
+			printf("msgrcv failed, rc=%d\n", rc);
+			return 1;
+		}
+
+		if (msg.mtype == 99){
+			//Exit message received
+			printf("Exit message received: %s\n", msg.mtext);
+			finish = true;
+		}else{
+			printf("received msg: %s\n", msg.mtext);
+			//Respond to the client
+			n = write(sock,"I got your queue message \n",26);
+			if (n < 0) {
+			  perror("ERROR writing to socket \n");
+			  finish = true;
+			}
+		}
+	}
+	return 0;
+}
+
+void createAndDetachThread(SDL_ThreadFunction fn, const char *name, void *data){
 	SDL_Thread *thread;
 
-	//Parameter definition
-	portno = 5001;
-	maxCon = 2;
+	thread = SDL_CreateThread(fn,name, data);
+	if (NULL == thread) {
+		printf("\nSDL_CreateThread failed: %s", SDL_GetError());
+	}
+	SDL_DetachThread(thread);
+
+}
+int manageNewConnection(int newsockfd){
+
+	int *newsock_dir;
+	int msgqid;
+
+	//Create the output messages queue for the socket
+	msgqid = msgget(IPC_PRIVATE, 0666|IPC_CREAT|IPC_EXCL);
+   if (msgqid < 0) {
+	  perror(strerror(errno));
+	  printf("failed to create message queue with msgqid = %d\n", msgqid);
+	  return 1;
+	}
+	socket_queue[newsockfd] = msgqid;
+
+	newsock_dir = (int *)malloc(sizeof(int));
+	*newsock_dir = newsockfd;
+
+	createAndDetachThread(doReading,"doReading", newsock_dir);
+	createAndDetachThread(doWriting,"doWriting", newsock_dir);
+}
+
+int openAndBindSocket(int port_number){
+	int sockfd;
+	struct sockaddr_in serv_addr;
 
 	// Opening the Socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -85,7 +175,7 @@ int main()
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
+	serv_addr.sin_port = htons(port_number);
 
 	// Binding server address with socket
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
@@ -95,40 +185,46 @@ int main()
 
 	//Listen through the socket
 	listen(sockfd,5);
-	clilen = sizeof(cli_addr);
+
+	return sockfd;
+}
+
+int main()
+{
+    cout << "Starting...\n";
+
+	int sockfd, newsockfd, port_number, max_con;
+	socklen_t cli_len;
+	struct sockaddr_in serv_addr, cli_addr;
+
+	cant_con = 0;
+
+	//Parameter definition
+	port_number = 5001;
+	max_con = 2;
+
+	sockfd = openAndBindSocket(port_number);
+
+	cli_len = sizeof(cli_addr);
 
 	 while (1) {
-		 cout << "CantCon: " << cantCon << " \n";
+		 cout << "CantCon: " << cant_con << " \n";
 
 		 //Accept connection from client
-		 newsockfd = accept(sockfd, (sockaddr *) &cli_addr, &clilen);
+		 newsockfd = accept(sockfd, (sockaddr *) &cli_addr, &cli_len);
 		 if (newsockfd < 0) {
 			perror("ERROR on accept");
-			exit(1);
+		 }else{
+
+			if (cant_con == max_con){
+				char* message = "ERROR: The Server has exceeded max number of connections. \n";
+				write(newsockfd,message,strlen(message));
+				close(newsockfd);
+			}else{
+				cant_con++;
+				manageNewConnection(newsockfd);
+			}
 		}
-
-	    if (cantCon == maxCon)
-	    {
-	    	char* message = "ERROR: The Server has exceeded max number of connections. \n";
-	    	write(newsockfd,message,strlen(message));
-	        close(newsockfd);
-	    }else{
-	    	cantCon++;
-
-	    	new_sock = (int *)malloc(sizeof(int));
-	    	*new_sock = newsockfd;
-
-	    	thread = SDL_CreateThread(doProcessing, (void *)new_sock);
-
-	        if (NULL == thread) {
-	            printf("\nSDL_CreateThread failed: %s", SDL_GetError());
-	        }
-	        /*else {
-	            //SDL_WaitThread(thread, &threadReturnValue);
-		    	//cantCon--;
-	            //printf("\nThread returned value: %d", threadReturnValue);
-	        }*/
-	    }
 	}
 
     return 1;
